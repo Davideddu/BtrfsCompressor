3cd0f018c666790e54504f277323b9191b5867b9
Davide Depau <davide@depau.eu>
2018-02-13 15:03:54 +0100

    Upgrade to Python3

--- compress.py
+++ compress.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import division
+from __future__ import division, print_function
 
-MAX_COMPRESSIBILITY = 0.7
+MAX_COMPRESSIBILITY = 0.6
 CHUNK_SIZE = 1*1024
 
 import os, sys, lzo, string, mimetypes
@@ -51,21 +51,25 @@ COMPRESSED_MIMES  =  ["application/x-par2",
                       "application/x-zoo"]
 
 def compressible_mime(fname):
-    t = str(mimetypes.guess_type(fname)[0])
+    t, c = mimetypes.guess_type(fname)
+    if not t:
+        return False
+    if c is not None:
+        return "never"
     if t.startswith("text"):
         return True
-    elif t in COMPRESSIBLE_MIMES:
+    if t in COMPRESSIBLE_MIMES:
         return True
-    elif t.startswith("audio") or t.startswith("video") or t.startswith("image"):
+    if t.startswith("audio") or t.startswith("video") or t.startswith("image"):
         return "never"
-    elif t in COMPRESSED_MIMES:
+    if t in COMPRESSED_MIMES:
         return "never"
-    return False
+    return True
 
 def istext(filename):
     with open(filename, "rt") as f:
         s = f.read(512)
-    text_characters = "".join(map(chr, range(32, 127)) + list("\n\r\t\b"))
+    text_characters = "".join(list(map(chr, list(range(32, 127)))) + list("\n\r\t\b"))
     _null_trans = string.maketrans("", "")
     if not s:
         # Empty files are considered text
@@ -98,20 +102,30 @@ def estimate_compressibility(path):
             return compression_factor(f.read(CHUNK_SIZE))
 
 def already_compressed(path):
+    if os.path.islink(path):
+        return True
     try:
         out = sp.check_output(["lsattr", path])
         if "c" in out:
             return True
+        return False
+    except sp.CalledProcessError:
         return False
     except Exception:
         import traceback; traceback.print_exc();
         return False
 
 def btrfs_compress(path):
-    p = sp.Popen(["chattr", "+c", path])
-    p.wait()
-    p = sp.Popen(["btrfs", "filesystem", "defragment", "-c", path])
-    p.wait()
+    try:
+        p = sp.Popen(["chattr", "+c", path])
+        p.wait()
+    except sp.CalledProcessError:
+        pass
+    try:
+        p = sp.Popen(["btrfs", "filesystem", "defragment", "-clzo", path])
+        p.wait()
+    except sp.CalledProcessError:
+        pass
 
 def scan_path(path):
     path = abspath(path)
@@ -127,16 +141,16 @@ def scan_path(path):
                 continue
             m = compressible_mime(f)
             if m == "never":
-                print "Not compressing", f, ", bad MIME"
+                print("Not compressing", f, ", bad MIME")
                 continue
             elif not m:
                 if not istext(f):
                     compressibility = estimate_compressibility(f)
                     if compressibility > MAX_COMPRESSIBILITY:
-                        print "Not compressing", f, ", compressibility ==", compressibility
+                        print("Not compressing", f, ", compressibility ==", compressibility)
                         continue
 
-            print "Compressing", f
+            print("Compressing", f)
             btrfs_compress(f)
 
 if __name__ == "__main__":
